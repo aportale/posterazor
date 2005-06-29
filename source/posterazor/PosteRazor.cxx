264bed632ce557678981781a2b43ec44256080c4
Alessandro Portale <alessandro@casaportale.de>
2005-06-29 22:26:59 +0000

    Added initial poster size getter and setter incl. optional aspect ratio calculation

--- source/posterazor/PosteRazor.cxx
+++ source/posterazor/PosteRazor.cxx
@@ -6,39 +6,47 @@ class PosteRazorImplementation: public PosteRazor
 private:
 	PosteRazorImageIO *m_imageIO;
 
-	char      m_fileName[1024];
-	char      m_documentName[1024];
+	char                   m_fileName[1024];
+	char                   m_documentName[1024];
 
-	int       m_piecesColumns;
-	int       m_piecesRows;
+	int                    m_piecesColumns;
+	int                    m_piecesRows;
 
-	int       m_sheetsColumns;
-	int       m_sheetsRows;
+	int                    m_sheetsColumns;
+	int                    m_sheetsRows;
 
-	float     m_overlapWidth;
-	float     m_overlapHeight;
+	float                  m_overlapWidth;
+	float                  m_overlapHeight;
 
-	float     m_posterWidth;
-	float     m_posterHeight;
+	double                 m_posterWidth;
+	double                 m_posterHeight;
 
-	ePaperFormats m_paperFormat;
-	enum eDistanceUnits m_distanceUnit;
-	eBorderPositions m_borderPosition;
-	ePaperOrientations m_paperOrientation;
+	ePaperFormats          m_paperFormat;
+	enum eDistanceUnits    m_distanceUnit;
+	eBorderPositions       m_borderPosition;
+	ePaperOrientations     m_paperOrientation;
+	bool                   m_preserveAspectRatio;
+	enum ePosterSizeModes  m_posterSizeMode;
+	bool                   m_lastEditedSizeWasWidth;
 
 public:
 	PosteRazorImplementation(const char* imgFileName)
 	{
 		m_imageIO = PosteRazorImageIO::CreatePosteRazorImageIO(imgFileName);
 
-		m_piecesColumns = 3;
-		m_piecesRows = 3;
+		m_piecesColumns           = 3;
+		m_piecesRows              = 3;
 
-		m_posterWidth = 0.0;
-		m_posterHeight = 0.0;
+		m_posterWidth             = 2.0;
+		m_posterHeight            = 2.0;
 
-		m_borderPosition = eBorderPositionRightBottom;
-		m_distanceUnit = eDistanceUnitCentimeter;
+		m_borderPosition          = eBorderPositionRightBottom;
+		m_distanceUnit            = eDistanceUnitCentimeter;
+		m_preserveAspectRatio     = true;
+		m_posterSizeMode          = ePosterSizeModePages;
+		m_lastEditedSizeWasWidth  = true;
+
+		CalculateAspectRatio();
 	}
 
 	~PosteRazorImplementation()
@@ -63,6 +71,66 @@ public:
 	void SetPaperFormatAndBorders(enum ePaperFormats, enum ePaperOrientations, double borderTop, double borderRight, double borderBottom, double borderLeft)
 	{
 	}
+
+	void CalculateAspectRatio()
+	{
+		double *source = m_lastEditedSizeWasWidth?&m_posterWidth:&m_posterHeight;
+		double *target = m_lastEditedSizeWasWidth?&m_posterHeight:&m_posterWidth;
+
+		int sourceReference = m_lastEditedSizeWasWidth?GetInputImageWidth():GetInputImageHeight();
+		int targetReference = m_lastEditedSizeWasWidth?GetInputImageHeight():GetInputImageWidth();
+		
+		double aspectRatio = (double)sourceReference/(double)targetReference;
+
+		*target = *source / aspectRatio;
+	}
+
+	void SetPreserveAspectRatio(bool preserveIt)
+	{
+		m_preserveAspectRatio = preserveIt;
+
+		if (m_preserveAspectRatio)
+			CalculateAspectRatio();
+	}
+
+	bool GetPreserveAspectRatio(void) {return m_preserveAspectRatio;}
+
+	void SetPosterWidthOrHeight(enum ePosterSizeModes mode, double widthOrHeight, bool width)
+	{
+		m_posterSizeMode = mode;
+		if (width)
+			m_posterWidth = widthOrHeight;
+		else
+			m_posterHeight = widthOrHeight;
+
+		m_lastEditedSizeWasWidth = width;
+
+		if (m_preserveAspectRatio)
+			CalculateAspectRatio();
+	}
+
+	void SetPosterWidth(enum ePosterSizeModes mode, double width)
+	{
+		SetPosterWidthOrHeight(mode, width, true);
+	}
+
+	void SetPosterHeight(enum ePosterSizeModes mode, double height)
+	{
+		SetPosterWidthOrHeight(mode, height, false);
+	}
+
+	double GetPosterWidth(enum ePosterSizeModes mode)
+	{
+		// Todo: mode dependant return
+		return ConvertBetweenDistanceUnits(m_posterWidth, eDistanceUnitCentimeter, m_distanceUnit);
+	}
+
+	double GetPosterHeight(enum ePosterSizeModes mode)
+	{
+		return ConvertBetweenDistanceUnits(m_posterHeight, eDistanceUnitCentimeter, m_distanceUnit);
+	}
+
+	enum ePosterSizeModes GetPosterSizeMode(void) {return m_posterSizeMode;}
 };
 
 PosteRazor* PosteRazor::CreatePosteRazor(const char* imgFileName)
--- source/posterazor/PosteRazor.h
+++ source/posterazor/PosteRazor.h
@@ -6,6 +6,21 @@
 class PosteRazor: public PaperFormats
 {
 public:
+	typedef enum eBorderPositions
+	{
+		eBorderPositionTopRight,
+		eBorderPositionRightBottom,
+		eBorderPositionBottomLeft,
+		eBorderPositionLeftTop
+	};
+
+	typedef enum ePosterSizeModes
+	{
+		ePosterSizeModeAbsolute,
+		ePosterSizeModePercentual,
+		ePosterSizeModePages
+	};
+
 	virtual ~PosteRazor() {};
 	static PosteRazor* CreatePosteRazor(const char* imgFileName);
 
@@ -23,14 +38,14 @@ public:
 
 	virtual void SetPaperFormatAndBorders(enum ePaperFormats, enum ePaperOrientations, double borderTop, double borderRight, double borderBottom, double borderLeft) = 0;
 	
+	virtual void SetPreserveAspectRatio(bool preserveIt) = 0;
+	virtual bool GetPreserveAspectRatio(void) = 0;
 
-	typedef enum eBorderPositions
-	{
-		eBorderPositionTopRight,
-		eBorderPositionRightBottom,
-		eBorderPositionBottomLeft,
-		eBorderPositionLeftTop
-	};
+	virtual void SetPosterWidth(enum ePosterSizeModes mode, double width) = 0;
+	virtual void SetPosterHeight(enum ePosterSizeModes mode, double height) = 0;
+	virtual double GetPosterWidth(enum ePosterSizeModes mode) = 0;
+	virtual double GetPosterHeight(enum ePosterSizeModes mode) = 0;
+	virtual enum ePosterSizeModes GetPosterSizeMode(void) = 0;
 };
 
 #endif
