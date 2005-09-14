deadb331122f2b11f4d3dc2734252b55d61b4a2e
Alessandro Portale <alessandro@casaportale.de>
2005-09-14 22:32:32 +0000

    big "const" avalanche

--- PosteRazor/source/posterazor/PosteRazorImageIO.h
+++ PosteRazor/source/posterazor/PosteRazorImageIO.h
@@ -13,23 +13,23 @@ public:
 	static PosteRazorImageIO* CreatePosteRazorImageIO();
 
 	virtual bool LoadImage(const char *imageFileName, char *errorMessage, int errorMessageSize) = 0;
-	virtual bool IsImageLoaded(void) = 0;
+	virtual bool IsImageLoaded(void) const = 0;
 
-	virtual int GetWidthPixels(void) = 0;
-	virtual int GetHeightPixels(void) = 0;
+	virtual int GetWidthPixels(void) const = 0;
+	virtual int GetHeightPixels(void) const = 0;
 
-	virtual double GetHorizontalDotsPerDistanceUnit(enum DistanceUnits::eDistanceUnits unit) = 0;
-	virtual double GetVerticalDotsPerDistanceUnit(enum DistanceUnits::eDistanceUnits unit) = 0;
+	virtual double GetHorizontalDotsPerDistanceUnit(enum DistanceUnits::eDistanceUnits unit) const = 0;
+	virtual double GetVerticalDotsPerDistanceUnit(enum DistanceUnits::eDistanceUnits unit) const = 0;
 
-	virtual double GetWidth(enum DistanceUnits::eDistanceUnits unit) = 0;
-	virtual double GetHeight(enum DistanceUnits::eDistanceUnits unit) = 0;
+	virtual double GetWidth(enum DistanceUnits::eDistanceUnits unit) const = 0;
+	virtual double GetHeight(enum DistanceUnits::eDistanceUnits unit) const = 0;
 
-	virtual void GetImageAsRGB(unsigned char **buffer, int &widthPixels, int &heightPixels) = 0;
+	virtual void GetImageAsRGB(unsigned char **buffer, int &widthPixels, int &heightPixels) const = 0;
 	
-	virtual int GetBitsPerPixel(void) = 0;
-	virtual enum eColorTypes GetColorDataType(void) = 0;
+	virtual int GetBitsPerPixel(void) const = 0;
+	virtual enum eColorTypes GetColorDataType(void) const = 0;
 
-	virtual int SavePoster(const char *fileName, enum ImageIOTypes::eImageFormats format, const PainterInterface *painter, int pagesCount, double widthCm, double heightCm) = 0;
+	virtual int SavePoster(const char *fileName, enum ImageIOTypes::eImageFormats format, const PainterInterface *painter, int pagesCount, double widthCm, double heightCm) const = 0;
 };
 
 #endif
