96e96179a41cf9971e94bff94d24f05eb34555fd
Alessandro Portale <alessandro@casaportale.de>
2006-01-17 22:18:34 +0000

    no message

--- PosteRazor/webpage/index.php
+++ PosteRazor/webpage/index.php
@@ -168,7 +168,7 @@
 			</div>
 			<div id="navigation">
 				<ul id="navigationlist"><li><a href="<?=linkParameters("news","")?>"><? text('News', 'Neuigkeiten'); ?></a></li><li><a href="<?=linkParameters("about","")?>"><? text('About', '&Uuml;ber'); ?></a></li><li><a href="<?=linkParameters("features","")?>"><? text('Features', 'Funktionen'); ?></a></li><li><a href="<?=linkParameters("screenshots","")?>"><? text('Screenshots', 'Bildschirmfotos'); ?></a></li><li><a class="external" href="http://sourceforge.net/project/showfiles.php?group_id=141842"><? text('Download', 'Herunterladen'); ?></a></li><li><a href="<?=linkParameters("thirdparty","")?>"><? text('Third&nbsp;party&nbsp;code', 'Code&nbsp;von&nbsp;Drittanbietern'); ?></a></li><li><a href="<?=linkParameters("license","")?>"><? text('License', 'Lizenz'); ?></a></li></ul>
-				<ul><li><a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=141842&amp;type=1" width="88" height="31" alt="SourceForge.net Logo" /></a></li><li><a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a></li></ul>
+				<ul><li><a href="http://sourceforge.net"><img src="_http://sourceforge.net/sflogo.php?group_id=141842&amp;type=1" width="88" height="31" alt="SF.net Logo" /></a></li><li><a href="http://validator.w3.org/check?uri=referer"><img src="_http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML" height="31" width="88" /></a></li></ul>
 			</div>
 			<div id="content">
 <?=writeContent();
