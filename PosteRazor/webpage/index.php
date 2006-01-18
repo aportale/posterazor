00ddee014a17f037e3bdfef6a55621090c76232d
Alessandro Portale <alessandro@casaportale.de>
2006-01-18 01:16:03 +0000

    Back to xhtml 1.0, because xtml 1.1 confused IE6

--- PosteRazor/webpage/index.php
+++ PosteRazor/webpage/index.php
@@ -150,15 +150,13 @@
 <?
 		};		
 	}
-
-echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\" ?>";	
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+	
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?
 	$textColor = "#330000";
 	$contentBackgroundColor = "#ffffee";
-?><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+?><html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 		<meta name="author" content="Alessandro Portale" />
