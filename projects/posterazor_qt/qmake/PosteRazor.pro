27dab17c5d6305ce66f538c3c3d262c7e48033ae
Alessandro Portale <alessandro@casaportale.de>
2006-11-19 22:43:29 +0000

    Connecting the Qt UI to the dialog controller

--- projects/posterazor_qt/qmake/PosteRazor.pro
+++ projects/posterazor_qt/qmake/PosteRazor.pro
@@ -1,10 +1,40 @@
 TEMPLATE = app
 
 SOURCES += \
-	../../../source/posterazor_ui_qt/PosteRazorDialog.cpp
+	../../../source/tools/PaperFormats.cxx \
+	../../../source/tools/UnitsOfLength.cxx \
+	../../../source/posterazor/PosteRazor.cxx \
+	../../../source/posterazor/PosteRazorImageIO.cxx \
+	../../../source/posterazor/PosteRazorPDFOutput.cxx \
+	../../../source/posterazor_ui_qt/PosteRazorDialog.cpp \
+	../../../source/posterazor_ui_common/PosteRazorDialogController.cpp
 
 HEADERS += \
-	../../../source/posterazor_ui_qt/PosteRazorDialog.h
+	../../../source/tools/ColorTypes.h \
+	../../../source/tools/ImageIOTypes.h \
+	../../../source/tools/PaintCanvasInterface.h \
+	../../../source/tools/PersistentPreferencesInterface.h \
+	../../../source/posterazor/PosteRazor.h \
+	../../../source/posterazor/PosteRazorImageIO.h \
+	../../../source/posterazor/PosteRazorPDFOutput.h \
+	../../../source/posterazor_ui_qt/PosteRazorDialog.h \
+	../../../source/posterazor_ui_common/PosteRazorDialogController.h
+
+INCLUDEPATH += \
+	../../../source/tools \
+	../../../source/posterazor \
+	../../../source/posterazor_ui_qt \
+	../../../source/posterazor_ui_common \
+	../../../thirdparty/FreeImage/Dist  \
+
+LIBS += \
+	../../../thirdparty/FreeImage/Dist/FreeImage.lib
+
+DEFINES += \
+	FREEIMAGE_LIB
+	
+DEFINES -= \
+	UNICODE
 
 UI_HEADERS_DIR = \
 	../../../source/posterazor_ui_qt
--- source/posterazor_ui_qt/PosteRazorDialog.h
+++ source/posterazor_ui_qt/PosteRazorDialog.h
@@ -3,13 +3,58 @@
 
 #include <QDialog>
 #include "ui_PosteRazorDialog.h"
+#include "PosteRazorDialogController.h"
 
-class PosteRazorDialog : public QDialog, private Ui::PosteRazorDialogUI
+class PosteRazorDialog : public QDialog, private Ui::PosteRazorDialogUI, public PosteRazorDialogInterface
 {
 	Q_OBJECT
 
 public:
 	PosteRazorDialog::PosteRazorDialog(QWidget *parent = 0, Qt::WFlags flags = 0);
+
+	virtual void SetUnitOfLength(UnitsOfLength::eUnitsOfLength unit);
+
+	virtual void SetPaperFormat(PaperFormats::ePaperFormats format);
+	virtual void SetPaperOrientation(PaperFormats::ePaperOrientations orientation);
+
+	virtual void SetPaperBorderTop(double border);
+	virtual void SetPaperBorderRight(double border);
+	virtual void SetPaperBorderBottom(double border);
+	virtual void SetPaperBorderLeft(double border);
+
+	virtual void SetCustomPaperWidth(double width);
+	virtual void SetCustomPaperHeight(double height);
+	virtual void SetUseCustomPaperSize(bool useIt);
+
+	virtual void SetOverlappingWidth(double width);
+	virtual void SetOverlappingHeight(double height);
+	virtual void SetOverlappingPosition(PosteRazorEnums::eOverlappingPositions position);
+
+	virtual void SetPosterWidth(PosteRazorEnums::ePosterSizeModes mode, double width);
+	virtual void SetPosterHeight(PosteRazorEnums::ePosterSizeModes mode, double height);
+	virtual void SetPosterSizeMode(PosteRazorEnums::ePosterSizeModes mode);
+	virtual void SetPosterHorizontalAlignment(PosteRazorEnums::eHorizontalAlignments alignment);
+	virtual void SetPosterVerticalAlignment(PosteRazorEnums::eVerticalAlignments alignment);
+
+	virtual void SetPosterOutputFormat(ImageIOTypes::eImageFormats format);
+	virtual void SetLaunchPDFApplication(bool launch);
+
+	virtual void UpdatePreview(void);
+	virtual void UpdateImageInfoFields(int imageWidthInPixels, int imageHeightInPixels, double imageWidth, double imageHeight, UnitsOfLength::eUnitsOfLength unitOfLength, double verticalDpi, double horizontalDpi, ColorTypes::eColorTypes colorType, int bitsPerPixel);
+
+private:
+	PosteRazor *m_posteRazor;
+	PosteRazorDialogController *m_posteRazorController;
+
+	void createConnections(void);
+	void createPosteRazorDialogController(void);
+	void populateUI(void);
+
+private slots:
+	void handleNextButtonClicked(void);
+	void handlePrevButtonClicked(void);
+	void handlePaperFormatComboBoxActivated(int index);
+	void updatePosterSizeGroupsState(void);
 };
 
 #endif
