6b5bf4f6f7ea0c713dc4adafce29ab4fad343cbd
Alessandro Portale <alessandro@casaportale.de>
2008-04-11 11:54:40 +0000

    VC6 is so 20th century!!

--- PosteRazor/projects/posterazor_fltk/vc6/buildClean.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call ..\..\..\thirdparty\FreeImage\clean.bat > nul
-msdev.exe ..\..\..\thirdparty\FreeImage\FreeImage.dsw /make "FreeImageLib - ALL" /rebuild 
-
-msdev.exe ..\..\..\thirdparty\fltk-1.1\visualc\fltk.dsw /make "fltk - ALL" /rebuild
-msdev.exe ..\..\..\thirdparty\fltk-1.1\visualc\fltk.dsw /make "fltkgl - ALL" /rebuild
-
-msdev.exe PosteRazor.dsw /make "PosteRazorUI - All" /rebuild
