07d1a85314f54d12ab449bbf269233cd4f4bc7f4
Alessandro Portale <alessandro@casaportale.de>
2008-05-10 21:53:25 +0000

    A script generates the installer and bin Win32 packages for us

---
+++ packaging/createWin32Release.cmd
@@ -0,0 +1,14 @@
+pushd ..\src
+rd /s /q Release
+rd /s /q CMakeFiles
+del CMakeCache.txt
+cmake . -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release || pause
+nmake clean || pause
+nmake || pause
+upx --best PosteRazor.exe
+popd
+ISCC.exe iss\posterazor.iss
+
+SET ARCHIVEFILENAME=PosteRazor-1.5-Win32.zip
+del %ARCHIVEFILENAME%
+7z a -tzip %ARCHIVEFILENAME% ..\src\PosteRazor.exe ..\CHANGES ..\LICENSE ..\README
