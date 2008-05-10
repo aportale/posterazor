a07d3b26b6faffbd8389ea8723f5c5db8df3d2ed
Alessandro Portale <alessandro@casaportale.de>
2008-05-10 13:44:32 +0000

    Adjusted paths

--- packaging/createLinuxRelease.sh
+++ packaging/createLinuxRelease.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-pushd src
+pushd ../src
 cmake .
 make clean
 make
 strip PosteRazor
 tar cfz PosteRazor-1.5-preview-Linux-x86.tgz PosteRazor CHANGES  README  LICENSE
-mv -f PosteRazor-1.5-preview-Linux-x86.tgz ../
 popd
+mv -f ../src/PosteRazor-1.5-preview-Linux-x86.tgz .
