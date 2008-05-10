1f612d9339b4549841fb12c4c0beed31301f878b
Alessandro Portale <alessandro@casaportale.de>
2008-05-10 19:51:14 +0000

    Moved CHANGES, README and LICENSE back from src into the root. Adapted createSourceRelease.cmd

--- packaging/createSourceRelease.cmd
+++ packaging/createSourceRelease.cmd
@@ -1,1 +1,3 @@
-7z a -tzip -r PosteRazor-1.4-Source.zip -x!.svn
+SET ARCHIVEFILENAME=PosteRazor-1.5-Source.zip
+del %ARCHIVEFILENAME%
+7z a -tzip -r %ARCHIVEFILENAME% -x!.svn ..\src ..\packaging ..\CHANGES ..\LICENSE ..\README
