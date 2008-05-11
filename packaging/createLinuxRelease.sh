d4ffcda91a60981c91c3decbf2401b23a4f649ca
Alessandro Portale <alessandro@casaportale.de>
2008-05-11 07:50:59 +0000

    CHANGES, README and LICENSE moved

--- packaging/createLinuxRelease.sh
+++ packaging/createLinuxRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ARCHIVEFILENAME=PosteRazor-1.6-Preview-Linux-x86.tgz
+export ARCHIVEFILENAME=PosteRazor-1.5-Linux-x86.tgz
 export SOURCEDIR=../src
 
 pushd $SOURCEDIR
@@ -8,6 +8,6 @@ cmake .
 make clean
 make
 strip PosteRazor
-tar cfz $ARCHIVEFILENAME PosteRazor CHANGES  README  LICENSE
+tar cfz $ARCHIVEFILENAME PosteRazor ../CHANGES  ../README  ../LICENSE
 popd
 mv -f $SOURCEDIR/$ARCHIVEFILENAME .
