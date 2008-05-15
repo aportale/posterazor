041b148e516fb6809e9063cdd7b7d39fd58bbcee
Alessandro Portale <alessandro@casaportale.de>
2008-05-15 23:01:48 +0000

    Not maintained

--- packaging/createOSXRelease.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pushd projects/posterazor_fltk/xcode2
-rm -fr build
-xcodebuild -project PosteRazor.xcodeproj -target PosteRazor -configuration Deployment clean build
-sh stripBinary.command
-hdiutil create -volname "PosteRazor" -ov -format UDZO -imagekey zlib-level=9 -srcfolder "build/Deployment" "../../../PosteRazor-1.4-OSX.dmg"
-popd
