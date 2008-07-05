ba047ca8d99e2d2da64756de99760892954f9d32
Alessandro Portale <alessandro@casaportale.de>
2008-07-05 14:17:47 +0000

    Only files from the SVN repository are now added to the source archive

--- packaging/createSourceRelease.cmd
+++ packaging/createSourceRelease.cmd
@@ -1,3 +1,14 @@
+SET LISTFILENAME=files.txt
 SET ARCHIVEFILENAME=PosteRazor-1.5-Source.zip
+del %LISTFILENAME%
 del %ARCHIVEFILENAME%
-7z a -tzip -r %ARCHIVEFILENAME% -x!.svn ..\src ..\packaging ..\CHANGES ..\LICENSE ..\README
+FOR /F %%i IN ('svn list -R ../') DO call :addtolistfile %%i
+pushd ..\
+7z a -tzip packaging\%ARCHIVEFILENAME% @packaging\%LISTFILENAME%
+popd
+goto :eof
+
+:addtolistfile
+set ENTRYTOADDTOLISTFILE=%1
+if not %ENTRYTOADDTOLISTFILE:~-1%==/ echo %ENTRYTOADDTOLISTFILE% >> %LISTFILENAME%
+goto :eof
