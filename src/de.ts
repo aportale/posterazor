071d6e967f873605c95458eda041bb1366633746
Alessandro Portale <alessandro@casaportale.de>
2008-08-21 22:07:26 +0000

    cleaned up

--- src/de.ts
+++ src/de.ts
@@ -1,273 +1,73 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="de_DE">
+<!DOCTYPE TS><TS version="2.0" language="de_DE">
 <context>
     <name>PosteRazorDialog</name>
     <message>
-        <location filename="QtPosteRazorDialog.ui" line="17"/>
-        <source>PosteRazor 1.5</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="177"/>
-        <source>?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="212"/>
-        <source>Input Image</source>
-        <translation type="unfinished">Ausgangsbild</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="280"/>
-        <source>...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="290"/>
-        <source>Image Informations</source>
-        <translation type="unfinished">Bildinformationen</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="309"/>
-        <source>Size (in pixels):</source>
-        <translation type="unfinished">Größe (in Pixeln):</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1151"/>
-        <source>Size:</source>
-        <translation type="unfinished">Größe:</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="336"/>
-        <source>Resolution:</source>
-        <translation type="unfinished">Auflösung:</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="353"/>
-        <source>Color type:</source>
-        <translation type="unfinished">Farbtyp:</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="399"/>
-        <source>Paper size</source>
-        <translation type="unfinished">Papierformat</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="458"/>
-        <source>Standard</source>
-        <translation type="unfinished">Standard</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="470"/>
-        <source>Format:</source>
-        <translation type="unfinished">Format:</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="496"/>
-        <source>Orientation:</source>
-        <translation type="unfinished">Ausrichtung:</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="527"/>
-        <source>Portrait</source>
-        <translation type="unfinished">Hochformat</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="534"/>
-        <source>Landscape</source>
-        <translation type="unfinished">Querformat</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="558"/>
-        <source>Custom</source>
-        <translation type="unfinished">Eigenes</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1131"/>
-        <source>Width:</source>
-        <translation type="unfinished">Breite:</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1141"/>
-        <source>Height:</source>
-        <translation type="unfinished">Höhe:</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="643"/>
-        <source>Borders (%1)</source>
-        <translation type="unfinished">Ränder (%1)</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="655"/>
-        <source>Top</source>
-        <translation type="unfinished">Oben</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="665"/>
-        <source>Left</source>
-        <translation type="unfinished">Links</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="675"/>
-        <source>Right</source>
-        <translation type="unfinished">Rechts</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="685"/>
-        <source>Bottom</source>
-        <translation type="unfinished">Unten</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="785"/>
-        <source>Overlapping size</source>
-        <translation type="unfinished">Maße der Überlappung</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="872"/>
-        <source>Overlapping position</source>
-        <translation type="unfinished">Überlappende Seiten</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="890"/>
-        <source>Top right</source>
-        <translation type="unfinished">Oben rechts</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="925"/>
-        <source>Top left</source>
-        <translation type="unfinished">Oben links</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="960"/>
-        <source>Bottom left</source>
-        <translation type="unfinished">Unten links</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="995"/>
-        <source>Bottom right</source>
-        <translation type="unfinished">Unten rechts</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1050"/>
-        <source>Poster size</source>
-        <translation type="unfinished">Postergröße</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1076"/>
-        <source>pages</source>
-        <translation type="unfinished">Seiten</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1093"/>
-        <source>Image size in percent</source>
-        <translation type="unfinished">Bildgröße in Prozent</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1100"/>
-        <source>Absolute image size</source>
-        <translation type="unfinished">Absolute Maße</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1114"/>
-        <source>Size in pages</source>
-        <translation type="unfinished">Größe in Seiten</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1266"/>
-        <source>Image alignment</source>
-        <translation type="unfinished">Ausrichtung des Bildes</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1536"/>
-        <source>Save the poster</source>
-        <translation type="unfinished">Das Poster speichern</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1548"/>
-        <source>Save poster</source>
-        <translation type="unfinished">Das Poster speichern</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1561"/>
-        <source>Open PDF after saving</source>
-        <translation type="unfinished">PDF nach Speicherung öffnen</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1622"/>
-        <source>Back</source>
-        <translation type="unfinished">Zurück</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.ui" line="1635"/>
-        <source>Next</source>
-        <translation type="unfinished">Weiter</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.cpp" line="198"/>
+        <location filename="mainwindow.cpp" line="203"/>
         <source>Monochrome</source>
         <translation type="unfinished">Monochrom</translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="199"/>
+        <location filename="mainwindow.cpp" line="204"/>
         <source>Gray scale</source>
         <translation type="unfinished">Graustufen</translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="200"/>
+        <location filename="mainwindow.cpp" line="205"/>
         <source>Palette</source>
         <translation type="unfinished">Palette</translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="201"/>
+        <location filename="mainwindow.cpp" line="206"/>
         <source>RGB</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="202"/>
+        <location filename="mainwindow.cpp" line="207"/>
         <source>RGBA</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="204"/>
+        <location filename="mainwindow.cpp" line="209"/>
         <source>CMYK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="271"/>
+        <location filename="controller.cpp" line="338"/>
         <source>All image formats</source>
-        <translation type="unfinished">Alle Bildformate</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="278"/>
+        <location filename="controller.cpp" line="345"/>
         <source>Load an input image</source>
-        <translation type="unfinished">Ein Ausgangsbild laden</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="431"/>
+        <location filename="controller.cpp" line="363"/>
+        <source>The Image &apos;%1&apos; could not be loaded.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="controller.cpp" line="364"/>
+        <source>Loading Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="controller.cpp" line="398"/>
         <source>Choose a filename to save under</source>
-        <translation type="unfinished">Das Poster speichern unter</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="445"/>
+        <location filename="controller.cpp" line="412"/>
         <source>The file &apos;%1&apos; already exists.
 Do you want to overwrite it?</source>
-        <translation type="unfinished">Die Datei &apos;%1&apos; extistiert bereits.
-Soll sie überschrieben werden?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="QtPosteRazorDialog.cpp" line="449"/>
+        <location filename="controller.cpp" line="416"/>
         <source>The File &quot;%1&quot; could not be saved.</source>
-        <translation type="unfinished">Die Datei &quot;%1&quot; konnte nicht gespeichert werden.</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.cpp" line="558"/>
-        <source>Loading Error</source>
-        <translation type="unfinished">Fehler</translation>
-    </message>
-    <message>
-        <location filename="QtPosteRazorDialog.cpp" line="558"/>
-        <source>The Image &apos;%1&apos; could not be loaded.</source>
-        <translation type="unfinished">Die Datei &quot;%1&quot; konnte nicht geladen werden.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
