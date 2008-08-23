6f22d3c44d394ec1080f8ef67c111fea8f8808ec
Alessandro Portale <alessandro@casaportale.de>
2008-08-23 17:51:05 +0000

    Qt-ification. Migrated translations.

--- src/zh_CN.ts
+++ src/zh_CN.ts
@@ -1,73 +1,279 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="zh_CN" sourcelanguage="en_US">
+<!DOCTYPE TS><TS version="1.1" language="zh_CN">
 <context>
     <name>PosteRazorDialog</name>
     <message>
-        <location filename="mainwindow.cpp" line="203"/>
+        <location filename="mainwindow.cpp" line="81"/>
+        <source>&amp;About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="83"/>
+        <source>Image Informations</source>
+        <translation type="unfinished">绝对大小</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="84"/>
+        <source>Color type:</source>
+        <translation type="unfinished">颜色类型:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="85"/>
+        <source>Resolution:</source>
+        <translation type="unfinished">分辨率:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="86"/>
+        <source>Size (in pixels):</source>
+        <translation type="unfinished">尺寸 (像素):</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="122"/>
+        <source>Size:</source>
+        <translation type="unfinished">尺寸:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="88"/>
+        <source>Input Image</source>
+        <translation type="unfinished">输入图片</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="89"/>
+        <source>Open PDF after saving</source>
+        <translation type="unfinished">保存后用默认的PDF程序打开</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="91"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="92"/>
+        <source>Next</source>
+        <translation type="unfinished">前进</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="119"/>
+        <source>Height:</source>
+        <translation type="unfinished">高度:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <source>Bottom left</source>
+        <translation type="unfinished">左下</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="95"/>
+        <source>Bottom right</source>
+        <translation type="unfinished">右下</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>Overlapping position</source>
+        <translation type="unfinished">位置调整</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="97"/>
+        <source>Top left</source>
+        <translation type="unfinished">左上</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>Top right</source>
+        <translation type="unfinished">右上</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="99"/>
+        <source>Overlapping size</source>
+        <translation type="unfinished">调整大小</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="121"/>
+        <source>Width:</source>
+        <translation type="unfinished">宽度:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <source>Bottom</source>
+        <translation type="unfinished">顶部</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <source>Borders (%1)</source>
+        <translation type="unfinished">边界 (%1)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="103"/>
+        <source>Left</source>
+        <translation type="unfinished">左边</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="104"/>
+        <source>Right</source>
+        <translation type="unfinished">右</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="105"/>
+        <source>Top</source>
+        <translation type="unfinished">顶部</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="108"/>
+        <source>Format:</source>
+        <translation type="unfinished">格式:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="109"/>
+        <source>Custom</source>
+        <translation type="unfinished">定制</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="110"/>
+        <source>Standard</source>
+        <translation type="unfinished">标准</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="111"/>
+        <source>Orientation:</source>
+        <translation type="unfinished">模板:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="112"/>
+        <source>Landscape</source>
+        <translation type="unfinished">画像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="113"/>
+        <source>Portrait</source>
+        <translation type="unfinished">画像</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="114"/>
+        <source>Paper size</source>
+        <translation type="unfinished">页面格式</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="117"/>
+        <source>Image alignment</source>
+        <translation type="unfinished">图片调整</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="120"/>
+        <source>pages</source>
+        <translation type="unfinished">页面</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="123"/>
+        <source>Absolute image size</source>
+        <translation type="unfinished">绝对大小</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>Poster size</source>
+        <translation type="unfinished">设置最终的图像大小</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="125"/>
+        <source>Size in pages</source>
+        <translation type="unfinished">以页面来计量尺寸：</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="126"/>
+        <source>Image size in percent</source>
+        <translation type="unfinished">图片尺寸</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="127"/>
+        <source>Back</source>
+        <translation type="unfinished">边界</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="128"/>
+        <source>Save poster</source>
+        <translation type="unfinished">保存</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="129"/>
+        <source>Save the poster</source>
+        <translation type="unfinished">另存为</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="263"/>
         <source>Monochrome</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">黑白</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="204"/>
+        <location filename="mainwindow.cpp" line="264"/>
         <source>Gray scale</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">灰度</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="205"/>
+        <location filename="mainwindow.cpp" line="265"/>
         <source>Palette</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">风景画</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="206"/>
+        <location filename="mainwindow.cpp" line="266"/>
         <source>RGB</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="207"/>
+        <location filename="mainwindow.cpp" line="267"/>
         <source>RGBA</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="209"/>
+        <location filename="mainwindow.cpp" line="269"/>
         <source>CMYK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="controller.cpp" line="338"/>
         <source>All image formats</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">所有的图像格式</translation>
     </message>
     <message>
         <location filename="controller.cpp" line="345"/>
         <source>Load an input image</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">输入一个图像</translation>
     </message>
     <message>
         <location filename="controller.cpp" line="363"/>
         <source>The Image &apos;%1&apos; could not be loaded.
 </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">无法读取文件 %1 。</translation>
     </message>
     <message>
         <location filename="controller.cpp" line="398"/>
         <source>Choose a filename to save under</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">另存为</translation>
     </message>
     <message>
         <location filename="controller.cpp" line="412"/>
         <source>The file &apos;%1&apos; already exists.
 Do you want to overwrite it?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">文件 &apos;%1&apos; 已经存在。
+要覆盖这个文件吗？</translation>
     </message>
     <message>
         <location filename="controller.cpp" line="416"/>
         <source>The File &quot;%1&quot; could not be saved.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">无法保存为文件 %1 。</translation>
     </message>
     <message>
         <location filename="controller.cpp" line="364"/>
         <source>Loading Error</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="366"/>
+        <source>Language name</source>
+        <translation type="unfinished">简体中文</translation>
     </message>
 </context>
 </TS>
