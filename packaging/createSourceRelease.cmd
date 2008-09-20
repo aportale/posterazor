c507e506675209e80d88294171c5b79f9428b31f
Alessandro Portale <alessandro@casaportale.de>
2008-09-20 16:03:28 +0000

    Only package src and readme files

--- packaging/createSourceRelease.cmd
+++ packaging/createSourceRelease.cmd
@@ -2,13 +2,14 @@ SET LISTFILENAME=files.txt
 SET ARCHIVEFILENAME=PosteRazor-1.9.5-Source.zip
 del %LISTFILENAME%
 del %ARCHIVEFILENAME%
-FOR /F %%i IN ('svn list -R ../') DO call :addtolistfile %%i
+FOR /F %%i IN ('svn list -R ../src/') DO call :addtolistfile %%i
 pushd ..\
 7z a -tzip packaging\%ARCHIVEFILENAME% @packaging\%LISTFILENAME%
+7z a -tzip packaging\%ARCHIVEFILENAME% CHANGES README LICENSE
 popd
 goto :eof
 
 :addtolistfile
 set ENTRYTOADDTOLISTFILE=%1
-if not %ENTRYTOADDTOLISTFILE:~-1%==/ echo %ENTRYTOADDTOLISTFILE% >> %LISTFILENAME%
+if not %ENTRYTOADDTOLISTFILE:~-1%==/ echo src/%ENTRYTOADDTOLISTFILE% >> %LISTFILENAME%
 goto :eof
