10df892cb60d3efed4568de60c5003f97a965c7b
Alessandro Portale <alessandro@casaportale.de>
2008-09-21 16:19:43 +0000

    test images

--- tests/testimages.cmd
+++ tests/testimages.cmd
@@ -1,7 +1,7 @@
 convert -size 3x1 xc:white -depth 16 ^
-    -fill "#f00" -draw "point 0,0" ^
-    -fill "#0f0" -draw "point 1,0" ^
-    -fill "#00f" -draw "point 2,0" ^
+    -fill "#df1234" -draw "point 0,0" ^
+    -fill "#12df34" -draw "point 1,0" ^
+    -fill "#1234df" -draw "point 2,0" ^
     rgb_48bpp.png
 
 convert -size 1x1 xc:white -depth 16 ^
@@ -11,16 +11,28 @@ convert -size 1x1 xc:white -depth 16 ^
 convert rgb_48bpp.png -depth 8 rgb_24bpp.png
 convert rgb_48bpp.png -colors 255 -depth 8 palette_8bpp.png
 
+convert -size 2x1  xc:none -depth 16 -colorspace Gray ^
+    -fill "#012301230123" -draw "point 0,0" ^
+    -fill "#ABCDABCDABCD" -draw "point 1,0" ^
+    grayscale_16bpp.png
+
 convert -size 3x2 xc:none -depth 16 ^
-    -fill "#ff0000ff" -draw "point 0,0" ^
-    -fill "#00ff00ff" -draw "point 1,0" ^
-    -fill "#0000ffff" -draw "point 2,0" ^
-    -fill "#ff000080" -draw "point 0,1" ^
-    -fill "#00ff0080" -draw "point 1,1" ^
-    -fill "#0000ff80" -draw "point 2,1" ^
+    -fill "#abcd00000000dcba" -draw "point 0,0" ^
+    -fill "#0000abcd0000dcba" -draw "point 1,0" ^
+    -fill "#00000000abcddcba" -draw "point 2,0" ^
+    -fill "#1234000000004321" -draw "point 0,1" ^
+    -fill "#0000123400004321" -draw "point 1,1" ^
+    -fill "#0000000012344321" -draw "point 2,1" ^
     rgba_64bpp.png
 
-convert rgba_64bpp.png -depth 8 rgba_32bpp.png
+convert -size 3x2 xc:none -depth 16 ^
+    -fill "#ab0000ba" -draw "point 0,0" ^
+    -fill "#00ab00ba" -draw "point 1,0" ^
+    -fill "#0000abba" -draw "point 2,0" ^
+    -fill "#12000021" -draw "point 0,1" ^
+    -fill "#00120021" -draw "point 1,1" ^
+    -fill "#00001221" -draw "point 2,1" ^
+    rgba_32bpp.png
 
 convert -size 4x1 xc:none -colorspace CMYK ^
     -fill cmyk(100,0,0,0) -draw "point 0,0" ^
