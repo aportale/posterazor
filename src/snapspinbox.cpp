873724dc41fedf00aa1b3765068efe2ba48243b2
Alessandro Portale <alessandro@casaportale.de>
2018-10-03 00:04:21 +0200

    Update copyrigt: 2005-2009 -> 2005-2018

--- src/snapspinbox.cpp
+++ src/snapspinbox.cpp
@@ -1,6 +1,6 @@
 /*
     PosteRazor - Make your own poster!
-    Copyright (C) 2005-2009 by Alessandro Portale
+    Copyright (C) 2005-2018 by Alessandro Portale
     http://posterazor.sourceforge.net/
 
     This file is part of PosteRazor
